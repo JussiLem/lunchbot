2406c52f1cf6059178a96b4f6090663c3283b507
Jussi Lemmetyinen <jussi.lemmetyinen@gofore.com>
2024-08-15 12:14:03 +0300

    fix(paths): removed codecov step

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,6 @@ import { awscdk, javascript } from 'projen'
 const project = new awscdk.AwsCdkTypeScriptApp({
   authorName: 'Jussi Lemmetyinen',
   cdkVersion: '2.1.0',
-  codeCov: true,
   defaultReleaseBranch: 'main',
   description: 'Serverless lunch bot',
   name: 'lunchbot',
