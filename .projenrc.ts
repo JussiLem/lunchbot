be132a2a71f32a1223e96b8616fcceb2a5235bcd
Jussi Lemmetyinen <jussi.lemmetyinen@gofore.com>
2024-08-15 14:24:03 +0300

    fix(release): removed npm plugin
    don't need to release anything to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -51,7 +51,6 @@ project.package.addField('release', {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
     '@semantic-release/github',
     [
       '@semantic-release/git',
